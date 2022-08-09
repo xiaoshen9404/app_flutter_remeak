7a1193f42ef08ed2fd5cff827ecb104878de3277
Dgeet4722 <jtom4722@163.com>
2022-08-10 00:19:20 +0800

    new url

--- lib/net/configs.dart
+++ lib/net/configs.dart
@@ -1,4 +1,4 @@
 class HttpConfig {
-  static const String baseUrl = "https://siot-saas.t.supor.com";
+  static const String baseUrl = "https://api.realdata.com";
   static const int receiveTimeout = 15000;
 }
